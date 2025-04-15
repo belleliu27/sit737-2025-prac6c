191e511e35ad4d9e1a44221d77a20cbace48885d
Belle <belleliu27@gmail.com>
2025-04-15 18:19:30 +1000

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Dockerized App!");
+  res.send("Updated version: Hello from Kubernetes!");
 });
 
 app.listen(PORT, () => {
